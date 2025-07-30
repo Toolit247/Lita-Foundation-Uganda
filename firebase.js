4322d181fcd71be7fc25b94c798cef9cfff36db3
Toolit247 <tambrosehenry@gmail.com>
2025-07-30 15:05:28 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAMnu2djtDKXMrTVKnppeCfKXRCy1KkEYk",
+  authDomain: "lita-platform-bffdc.firebaseapp.com",
+  projectId: "lita-platform-bffdc",
+  storageBucket: "lita-platform-bffdc.appspot.com",
+  messagingSenderId: "387943226211",
+  appId: "1:387943226211:web:0127c409e0f880df391156",
+  measurementId: "G-44JEYP8ZHD"
+};
+
+firebase.initializeApp(firebaseConfig);
