4909c05fadc5818d1da790b5e67793906df724f2
Toolit247 <tambrosehenry@gmail.com>
2025-09-26 12:44:26 +0300

    Delete js/firebase.js

--- js/firebase.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyAMnu2djtDKXMrTVKnppeCfKXRCy1KkEYk",
-  authDomain: "lita-platform-bffdc.firebaseapp.com",
-  projectId: "lita-platform-bffdc",
-  storageBucket: "lita-platform-bffdc.appspot.com",
-  messagingSenderId: "387943226211",
-  appId: "1:387943226211:web:0127c409e0f880df391156"
-};
-firebase.initializeApp(firebaseConfig);
