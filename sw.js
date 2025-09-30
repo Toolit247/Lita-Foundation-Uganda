36e00b6c933f798666955ed2ab88a2a484c9add0
Toolit247 <tambrosehenry@gmail.com>
2025-09-30 09:26:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,27 @@
+// Simple cache-first service worker
 const CACHE_NAME = "lita-cache-v1";
 const urlsToCache = [
-  "index.html",
-  "signin.html",
-  "admin.html",
-  "member.html",
-  "styles.css",
-  "tree.js",
-  "admin.js",
-  "member.js",
-  "manifest.json"
+  "./",
+  "./index.html",
+  "./signin.html",
+  "./admin.html",
+  "./member.html",
+  "./styles.css",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 // Install SW
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Fetch requests
+// Fetch cached resources
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
@@ -27,16 +30,14 @@ self.addEventListener("fetch", event => {
   );
 });
 
-// Update old cache
+// Activate and clean old caches
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(cacheNames =>
       Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
       )
     )
   );
