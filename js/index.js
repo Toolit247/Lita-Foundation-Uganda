819debd54acaef246eb7d7acb06aa0e1b1d87ada
Toolit247 <tambrosehenry@gmail.com>
2025-10-02 16:52:26 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// js/index.js
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.style.display = "inline-block";
+});
+
+installBtn.addEventListener("click", async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === "accepted") {
+      console.log("PWA installed");
+    }
+    deferredPrompt = null;
+    installBtn.style.display = "none";
+  }
+});
